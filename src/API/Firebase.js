78575bd6862f73ffc54092aa3136daf93cec4e59
haeseok <dawn147@snu.ac.kr>
2023-07-28 12:23:13 +0900

    login. logout 구현

--- src/API/Firebase.js
+++ src/API/Firebase.js
@@ -1,14 +1,39 @@
-import { initializeApp } from "firebase/app";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+  onAuthStateChanged,
+} from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  apiKey: "AIzaSyCy2W3J0MehuPjYS3YGNNC4LJOFFJctMXA",
+  authDomain: "shoppy-45e82.firebaseapp.com",
+  databaseURL: "https://shoppy-45e82-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "shoppy-45e82",
 };
 
 const app = initializeApp(firebaseConfig);
-const provider = new GoogleAuthProvider();
 const auth = getAuth();
+const provider = new GoogleAuthProvider();
 
+export async function login() {
+  return signInWithPopup(auth, provider)
+    .then((result) => {
+      const user = result.user;
+      console.log(user);
+      return user
+    })
+    .catch(e => console.log(e))
+}
+
+export async function logout() {
+  return signOut(auth).then(() => null);
+}
+
+export function onUserStateChange(callback) {
+  onAuthStateChanged(auth, (user) => {
+    callback(user);
+  });
+}
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,16 +1,52 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import {FaPen} from 'react-icons/fa'
-import styles from './Navbar.module.css'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { FaPen } from "react-icons/fa";
+import styles from "./Navbar.module.css";
+import {
+  login as Login,
+  logout as Logout,
+  onUserStateChange,
+} from "../../API/Firebase";
 
 export default function Navbar() {
+  const [user, setUser] = useState();
+  const [login, setLogin] = useState("Login");
+
+  useEffect(() => {
+    onUserStateChange((user) => {
+      setUser(user);
+    });
+  }, []);
+  
+  const handleLoginout = () => {
+    if (login === "Login") {
+      Login()
+        .then((u) => setUser(u))
+        .catch((e) => console.log(e));
+      setLogin("Logout");
+    } else {
+      Logout().then(() => setUser(null));
+      setLogin("Login");
+    }
+  };
   return (
     <div className={styles.container}>
-      <Link to='./products'>Products</Link>
-      <Link to='./cart'>My Cart</Link>
-      <Link to='./products/new'><FaPen /></Link>
-      <Link to='./'>Login</Link>
+      <Link to="./products">Products</Link>
+      <Link to="./cart">My Cart</Link>
+      <Link to="./products/new">
+        <FaPen />
+      </Link>
+      {user ? (
+        <span className={styles.user}>
+          <img src={user.photoURL} alt="" />
+          {user.displayName}
+        </span>
+      ) : (
+        <></>
+      )}
+      <button className={styles.login} onClick={handleLoginout}>
+        {login}
+      </button>
     </div>
   );
 }
-
