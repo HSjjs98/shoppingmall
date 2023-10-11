05f5c495600f3f03cf143b3ab8ad4564617cfaab
James <dawn147@snu.ac.kr>
2023-10-11 12:44:43 +0900

    Product-Detail impl

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import AllProducts from './AllProducts/AllProducts';
+import Banner from '../Components/Banner/Banner';
 
 export default function Home() {
   return (
-    <div>
+    <>
+      <Banner />
       <AllProducts />
-    </div>
+    </>
   );
 }
 
